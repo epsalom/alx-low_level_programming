2806a35e5bc4a6d57dd4e43ea15c66520cf809b5
epsalom <epsalomdelta@gmail.com>
2023-02-27 05:00:17 -0800

    pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,33 +1,18 @@
 #include "main.h"
 
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
-void rev_string(char *s)
+ * string_toupper - a function that changes all lower of a string to upper
+ * @n: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *n)
 {
-	int i, c, k;
-	char *a, aux;
+	int i;
 
-	a = s;
-
-	while (s[c] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		c++;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+	return (n);
 }
